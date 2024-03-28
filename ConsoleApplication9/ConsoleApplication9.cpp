30ddcc9ab9e5170d4799f06a235c9278fee199ca
MykytaMendukov <nikitamendukov@gmail.com>
2024-03-28 18:00:33 +0200

    All done

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -9,14 +9,18 @@ using std::endl;
 using std::vector;
 using std::ostream;
 
-class Pizza {
+
+class Dish {
 private:
+    string name;
     vector<string> ingredients;
     double cost;
 
 public:
-    Pizza(double cost_) : cost(cost_) {}
-    Pizza() : cost(0.0) {}
+    Dish(string name_, double cost_) : name(name_), cost(cost_) {}
+    Dish() : name("None"), cost(0.0) {}
+
+    ~Dish() { name = ""; cost = 0.0; ingredients.clear(); }
 
     void addIngredients(string obj) {
         string delimiter = ", ";
@@ -24,18 +28,34 @@ public:
         string token;
         while ((pos = obj.find(delimiter)) != string::npos) {
             token = obj.substr(0, pos);
-            cout << token << std::endl;
+            ingredients.push_back(token);
             obj.erase(0, pos + delimiter.length());
         }
-        cout << obj << endl;
+        ingredients.push_back(obj);
     }
+
+    friend ostream& operator <<(ostream& out, Dish& obj);
+
 };
+
+
+ostream& operator <<(ostream& out, Dish& obj) {
+
+    out << obj.name << " ingredients: " << endl;
+    for (string i : obj.ingredients) {
+        cout << i << ", ";
+    }
+    cout << endl;
+    out << "Cost: " << obj.cost << endl;
+    return out;
+}
 
 class Cook {
 private:
     string name;
     string speciality;
     double experience;
+    vector<Dish> dishes;
 
 public:
     Cook(string name_, string speciality_, double experience_)
@@ -43,19 +63,48 @@ public:
 
     Cook() : name("None"), speciality("None"), experience(0.0) {}
 
+    ~Cook() { name = ""; speciality = ""; experience = 0.0; }
+
+    void addDish(Dish& dish) {
+        dishes.push_back(dish);
+    }
+
+    void seeDishes() {
+        cout << "Chefs' dishes: " << endl;
+        int i = 1;
+        for (Dish& dish : dishes) {
+            cout << i << ". ";
+            cout << dish << endl;
+            i++;
+        }
+    }
+
     friend ostream& operator<<(ostream& out, const Cook& cook);
 };
 
 ostream& operator<<(ostream& out, const Cook& cook) {
     out << "Name: " << cook.name << endl;
     out << "Speciality: " << cook.speciality << endl;
-    out << "Experience: " << cook.experience << endl;
+    out << "Experience: " << cook.experience << " years" << endl;
     return out;
 }
 
+
 int main() {
-    Pizza pizza(16.7);
-    pizza.addIngredients("Dough, Ketchup, Cheese");
+
+    Cook cook1("Mykola Mykolaichuk", "Cher", 7.6);
+
+    Dish Pizza("Pizza", 75);
+    Pizza.addIngredients("Dough, Ketchup, Cheese, Chicken Meat, Paprika");
+
+    Dish Salad("Salad", 60);
+    Salad.addIngredients("Lettuce, Sauce, Tomatoes, Feta Cheese");
+
+    cook1.addDish(Pizza);
+    cook1.addDish(Salad);
+
+    cout << cook1 << endl;
+    cook1.seeDishes();
 
     return 0;
 }
